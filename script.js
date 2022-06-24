cacd4787f19756c2ee9dace4cbc0ca521edf60c4
SlowSlyme <100006452+SlowSlyme@users.noreply.github.com>
2022-06-24 11:57:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let isGameOver = false;
 let position = 0;
 
 function handleKeyUp(event) {
-  if (event.keyCode === 32) {
+  if (event.keyCode === 65, 32) {
     if (!isJumping) {
       jump();
     }
